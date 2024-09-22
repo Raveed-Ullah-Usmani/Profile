354a9918e17409f41fdf9ba1da2e018ee1da4e27
Raveed <raveedusmani@gmail.com>
2024-09-22 21:30:46 +0500

    Population through Json object

--- script.js
+++ script.js
@@ -1,52 +1,27 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const nav = document.querySelector(".nav");
-
-hamburgerMenu.addEventListener("click", () => {
-    nav.classList.toggle("active")
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Home section
-    document.getElementById('home-name').textContent = 'Raveed Ullah Usmani';
-    document.getElementById('home-role').textContent = 'Computer Undergraduate | Hobbyist Game Developer';
-    document.getElementById('home-intro').textContent = 'I would love to connect 😄';
-    document.getElementById('github-link').href = 'https://github.com/Raveed-Ullah-Usmani/';
-    document.getElementById('linkedin-link').href = 'https://pk.linkedin.com/in/raveed-ullah-usmani-394106247';
-    document.getElementById('email-link').href = 'mailto:raveedusmani@gmail.com';
-    document.getElementById('resume-link').href = 'Raveed_Ullah_Usmani_resume.pdf';
-
-    // About section
-    document.getElementById('about-image').src = 'Profile.png';
-    document.getElementById('about-text1').textContent = 'I am a final-year Computer Science student at Information Technology University, with a deep passion for game development and VR experiences, which I actively pursue as a hobby.';
-    document.getElementById('about-text2').textContent = 'My academic interests extend to Machine Learning, Deep Learning, Algorithms, LLMs, and Speech Processing and the list is still growing.';
-
-    // Skills section
-    const skills = [
+const data = {
+    home: {
+        name: 'Raveed Ullah Usmani',
+        role: 'Computer Undergraduate | Hobbyist Game Developer',
+        intro: 'I would love to connect 😄',
+        githubLink: 'https://github.com/Raveed-Ullah-Usmani/',
+        linkedinLink: 'https://pk.linkedin.com/in/raveed-ullah-usmani-394106247',
+        emailLink: 'mailto:raveedusmani@gmail.com',
+        resumeLink: 'Raveed_Ullah_Usmani_resume.pdf'
+    },
+    about: {
+        image: 'Profile.png',
+        text1: 'I am a final-year Computer Science student at Information Technology University, with a deep passion for game development and VR experiences, which I actively pursue as a hobby.',
+        text2: 'My academic interests extend to Machine Learning, Deep Learning, Algorithms, LLMs, and Speech Processing and the list is still growing.'
+    },
+    skills: [
         { icon: 'fab fa-unity', name: 'Unity', proficiency: '80%' },
         { icon: 'fab fa-android', name: 'Flutter', proficiency: '70%' },
         { icon: 'fab fa-html5', name: 'HTML/CSS', proficiency: '90%' },
         { icon: 'fab fa-node-js', name: 'Node.js', proficiency: '75%' },
         { icon: 'fas fa-database', name: 'MongoDB', proficiency: '65%' },
         { icon: 'fab fa-python', name: 'Python', proficiency: '85%' }
-    ];
-
-    const skillsContent = document.getElementById('skills-content');
-    skills.forEach(skill => {
-        const skillItem = document.createElement('div');
-        skillItem.className = 'skill-item';
-        skillItem.innerHTML = `
-            <i class="${skill.icon}"></i>
-            <h3>${skill.name}</h3>
-            <div class="proficiency-bar">
-                <div class="proficiency-bg"></div>
-                <div class="proficiency-fill" style="width: ${skill.proficiency};"></div>
-            </div>
-        `;
-        skillsContent.appendChild(skillItem);
-    });
-
-    // Experience section
-    const experiences = [
+    ],
+    experience: [
         {
             title: 'Teaching Assistant',
             location: 'Information Technology University | August 2023 - Present',
@@ -57,25 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
             location: 'Mindstorm Studios | June 2023 - August 2023',
             description: 'Focused on mastering game design principles, UI design, and dynamic feedback techniques to create engaging user experiences. Acquired proficiency in Unity and gained hands-on experience in asset creation using Blender. Participated in Rookie Game Jam 2023, where I successfully submitted a game that demonstrated my creativity and practical skills in game development.'
         }
-    ];
-
-    const experienceContent = document.getElementById('experience-content');
-    experiences.forEach(exp => {
-        const expItem = document.createElement('div');
-        expItem.className = 'experience-item';
-        expItem.innerHTML = `
-            <div class="experience-info">
-                <h2>${exp.title}</h2>
-                <p class="location">${exp.location}</p>
-                <p class="description">${exp.description}</p>
-            </div>
-            <div class="vertical-line"></div>
-        `;
-        experienceContent.appendChild(expItem);
-    });
-
-    // Projects section
-    const projects = [
+    ],
+    projects: [
         {
             title: 'Immerse & Chill',
             type: 'Course',
@@ -104,11 +62,63 @@ document.addEventListener('DOMContentLoaded', () => {
             description: 'A social media app developed using Flutter and Firebase. It features real-time communication and data management, showcasing skills in mobile app development and cloud-based databases.',
             link: 'https://github.com/Raveed-Ullah-Usmani'
         }
-    ];
-    
+    ],
+    contact: {
+        githubLink: 'https://github.com/Raveed-Ullah-Usmani/',
+        linkedinLink: 'https://pk.linkedin.com/in/raveed-ullah-usmani-394106247',
+        emailLink: 'mailto:raveedusmani@gmail.com'
+    }
+};
+document.addEventListener('DOMContentLoaded', () => {
+    // Home section
+    document.getElementById('home-name').textContent = data.home.name;
+    document.getElementById('home-role').textContent = data.home.role;
+    document.getElementById('home-intro').textContent = data.home.intro;
+    document.getElementById('github-link').href = data.home.githubLink;
+    document.getElementById('linkedin-link').href = data.home.linkedinLink;
+    document.getElementById('email-link').href = data.home.emailLink;
+    document.getElementById('resume-link').href = data.home.resumeLink;
 
+    // About section
+    document.getElementById('about-image').src = data.about.image;
+    document.getElementById('about-text1').textContent = data.about.text1;
+    document.getElementById('about-text2').textContent = data.about.text2;
+
+    // Skills section
+    const skillsContent = document.getElementById('skills-content');
+    data.skills.forEach(skill => {
+        const skillItem = document.createElement('div');
+        skillItem.className = 'skill-item';
+        skillItem.innerHTML = `
+            <i class="${skill.icon}"></i>
+            <h3>${skill.name}</h3>
+            <div class="proficiency-bar">
+                <div class="proficiency-bg"></div>
+                <div class="proficiency-fill" style="width: ${skill.proficiency};"></div>
+            </div>
+        `;
+        skillsContent.appendChild(skillItem);
+    });
+
+    // Experience section
+    const experienceContent = document.getElementById('experience-content');
+    data.experience.forEach(exp => {
+        const expItem = document.createElement('div');
+        expItem.className = 'experience-item';
+        expItem.innerHTML = `
+            <div class="experience-info">
+                <h2>${exp.title}</h2>
+                <p class="location">${exp.location}</p>
+                <p class="description">${exp.description}</p>
+            </div>
+            <div class="vertical-line"></div>
+        `;
+        experienceContent.appendChild(expItem);
+    });
+
+    // Projects section
     const projectsContent = document.getElementById('projects-content');
-    projects.forEach(project => {
+    data.projects.forEach(project => {
         const projectItem = document.createElement('div');
         projectItem.className = 'project-item';
         projectItem.innerHTML = `
@@ -121,7 +131,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Contact section
-    document.getElementById('contact-github-link').href = 'https://github.com/Raveed-Ullah-Usmani/';
-    document.getElementById('contact-linkedin-link').href = 'https://pk.linkedin.com/in/raveed-ullah-usmani-394106247';
-    document.getElementById('contact-email-link').href = 'mailto:raveedusmani@gmail.com';
+    document.getElementById('contact-github-link').href = data.contact.githubLink;
+    document.getElementById('contact-linkedin-link').href = data.contact.linkedinLink;
+    document.getElementById('contact-email-link').href = data.contact.emailLink;
+});
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+
+hamburgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("active");
 });
